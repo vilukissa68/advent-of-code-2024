e5d670cc2e83905e3292e44e7f966cc5ba266548
Väinö Granat <vaino-waltteri.granat@tuni.fi>
2024-12-22 17:49:07 +0200

    Day 7 - part 1 completed

---
+++ elisp/day7.el
@@ -0,0 +1,47 @@
+;;; day7.el --- Description -*- lexical-binding: t; -*-
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+(defun generate-permutations (functions length)
+  "Generate all permutations of FUNCTIONS with LENGTH."
+  (if (= length 0)
+      (list '())
+    (let (result)
+      (dolist (fn functions)
+	(dolist (perm (generate-permutations functions (1- length)))
+	  (push (cons fn perm) result)))
+      result)))
+
+(defun apply-functions-to-values (functions values)
+  "Apply all the FUNCTIONS in a list between VALUES."
+  (if (or (null functions) (= (length values) 1))
+      (car values)
+    (let* ((result (funcall (car functions) (car values) (cadr values)))
+	   (remaining-values (cons result (cddr values))))
+      (apply-functions-to-values (cdr functions) remaining-values))))
+
+(defun day7-1 ()
+  "Solution for Day 7 part 1."
+  (interactive)
+  (let* ((input (s-trim (read-string "Enter input: ")))
+	 (lines (s-split "\n" input))
+	 (result (mapcar (lambda (line)
+			   (let* ((split (s-split ": " line))
+				  (target (string-to-number (car split)))
+				  (numbers (mapcar 'string-to-number (s-split " " (cadr split))))
+				  (no-operations (- (length numbers) 1))
+				  (operations (generate-permutations '(+ *) no-operations))
+				  (results (mapcar (lambda (operation)
+						     (apply-functions-to-values operation numbers))
+						   operations)))
+			     (message "Operations: %s" results)
+			     (if (member target results)
+				 target 0)))
+			 lines)))
+    (message "Result: %d" (apply '+ result))))
+
+(provide 'day7)
