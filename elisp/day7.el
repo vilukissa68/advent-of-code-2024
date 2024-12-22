6eb06cb40526ce2012dfb1ae2f93460fde52811e
Väinö Granat <vaino-waltteri.granat@tuni.fi>
2024-12-22 21:25:12 +0200

    Day 7 - part 2 completed

--- elisp/day7.el
+++ elisp/day7.el
@@ -24,6 +24,33 @@
 	   (remaining-values (cons result (cddr values))))
       (apply-functions-to-values (cdr functions) remaining-values))))
 
+
+(defun concatenate-nums (a b)
+  "Concatenate numbers A and B."
+  (string-to-number (concat
+		     (number-to-string a)
+		     (number-to-string b))))
+
+(defun day7-2 ()
+  "Solution for Day 7 part 2."
+  (interactive)
+  (let* ((input (s-trim (read-string "Enter input: ")))
+	 (lines (s-split "\n" input))
+	 (result (mapcar (lambda (line)
+			   (let* ((split (s-split ": " line))
+				  (target (string-to-number (car split)))
+				  (numbers (mapcar 'string-to-number (s-split " " (cadr split))))
+				  (no-operations (- (length numbers) 1))
+				  (operations (generate-permutations '(+ * concatenate-nums) no-operations))
+				  (results (mapcar (lambda (operation)
+						     (apply-functions-to-values operation numbers))
+						   operations)))
+			     (if (member target results)
+				 target 0)))
+			 lines)))
+    (message "Result: %d" (apply '+ result))))
+
+
 (defun day7-1 ()
   "Solution for Day 7 part 1."
   (interactive)
@@ -38,7 +65,6 @@
 				  (results (mapcar (lambda (operation)
 						     (apply-functions-to-values operation numbers))
 						   operations)))
-			     (message "Operations: %s" results)
 			     (if (member target results)
 				 target 0)))
 			 lines)))
